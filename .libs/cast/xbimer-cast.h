8cc30138f5745b89578cf6416dce3be52e4aa0ea
beanjs <502554248@qq.com>
2021-04-20 22:52:07 +0800

    1.init `DGGraphix`,`DGLib`,`Graphix`,`GSRoot`,`TextEngine`

---
+++ .libs/cast/xbimer-cast.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#ifndef __XBIMER_CAST_H__
+#define __XBIMER_CAST_H__
+
+#if !defined(ARCHICAD_NONE)
+
+#include "common.h"
+
+#if defined(ARCHICAD_22)
+#include "ac22.h"
+#endif
+
+#if defined(ARCHICAD_23)
+#include "ac23.h"
+#endif
+
+#endif
+
+#endif // !__XBIMER_CAST_H__
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# xbimer-modules
-Python module encapsulation based on C++ SDK
+<h1 align="center" >xBIMer Modules</h1>
+<p align="center">Python module encapsulation based on C++ SDK</p>
+
+<p align="center">
+<a href="https://github.com/xbimer/xbimer-docs">Read Documentation</a>
+</p>
